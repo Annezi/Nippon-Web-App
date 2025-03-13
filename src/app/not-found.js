2c5ff29a3e17b5988961a31cbcfc233d188f788f
Anna Axmatova <ax.annushka@yandex.ru>
2025-03-13 19:23:36 +0300

    [UPDATE] Фикс страниц

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,24 +1,24 @@
-// "use client";
-// import dynamic from 'next/dynamic';
-// import { useState } from 'react';
-// import Header from "./components/Navigation/Navbar/Navbar";
-// import './components/UI/SocialMadiaLinks/SocialMadiaLinks.css';
+"use client";
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import Header from "./components/Navigation/Navbar/Navbar";
+import './components/UI/SocialMadiaLinks/SocialMadiaLinks.css';
 
-// const InProgress = dynamic(() => import("./components/InProgress/InProgress"), { ssr: false });
-// const AlarmPopup = dynamic(() => import('./components/UI/Popups/Alarm/Alarm_Popup'), { ssr: false });
+const InProgress = dynamic(() => import("./components/InProgress/InProgress"), { ssr: false });
+const AlarmPopup = dynamic(() => import('./components/UI/Popups/Alarm/Alarm_Popup'), { ssr: false });
 
-// export default function NotFound() {
-// 	const [isAlarmPopupOpen, setIsAlarmPopupOpen] = useState(true);
+export default function NotFound() {
+	const [isAlarmPopupOpen, setIsAlarmPopupOpen] = useState(true);
 
-// 	const handleAlarmPopupClose = () => {
-// 		setIsAlarmPopupOpen(false);
-// 	};
+	const handleAlarmPopupClose = () => {
+		setIsAlarmPopupOpen(false);
+	};
 
-// 	return (
-// 		<div className="ContainerBox">
-// 			{isAlarmPopupOpen && <AlarmPopup onClose={handleAlarmPopupClose} />}
-// 			<Header />
-// 			<InProgress />
-// 		</div>
-// 	);
-// }
+	return (
+		<div className="ContainerBox">
+			{isAlarmPopupOpen && <AlarmPopup onClose={handleAlarmPopupClose} />}
+			<Header />
+			<InProgress />
+		</div>
+	);
+}
