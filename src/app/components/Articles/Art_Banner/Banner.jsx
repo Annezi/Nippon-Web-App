470dc9bc352e8ceefa89cdb8fd256806d98444a6
Anna Axmatova <ax.annushka@yandex.ru>
2025-06-25 22:21:00 +0300

    [FIX] - animation, adaptive

--- src/app/components/Articles/Art_Banner/Banner.jsx
+++ src/app/components/Articles/Art_Banner/Banner.jsx
@@ -12,31 +12,29 @@ export default function Banner({ tags, title, description, readTime, publishDate
 	const breadcrumbItems = generateBreadcrumbs(pathname);
 
 	return (
-		<ScrollReveal index={0}>
-			<div className="banner-container" style={{ backgroundImage: `url(${cover})` }}>
-				<div className="ban-infoSub">
-					<div className="ban-breadcrumbs">
-						<Breadcrumbs items={breadcrumbItems} />
-					</div>
-					<div className="ban-readTime">
-						<div className="readTime-title text-paragraph-m">Время чтения:</div>
-						<div className="readTime-time text-paragraph-m">{readTime}</div>
-					</div>
+		<div className="banner-container" style={{ backgroundImage: `url(${cover})` }}>
+			<div className="ban-infoSub">
+				<div className="ban-breadcrumbs">
+					<Breadcrumbs items={breadcrumbItems} />
 				</div>
-				<div className="ban-infoMain">
-					<div className="infoMain-ArticleInfo">
-						<div className="ArticleInfo-tags">
-							{tags.map((tag, index) => (
-								<Tag key={index} text={tag} size='small' theme='white' isActive={false} />
-							))}
-						</div>
-						<div className="ArticleInfo-title text-title-l">{title}</div>
-						<div className="ArticleInfo-description text-paragraph-m">{description}</div>
-					</div>
-					<div className="infoMain-publushDate text-paragraph-s">{publishDate}</div>
+				<div className="ban-readTime">
+					<div className="readTime-title text-paragraph-m">Время чтения:</div>
+					<div className="readTime-time text-paragraph-m">{readTime}</div>
 				</div>
-				<div className="darken-bg"></div>
 			</div>
-		</ScrollReveal>
+			<div className="ban-infoMain">
+				<div className="infoMain-ArticleInfo">
+					<div className="ArticleInfo-tags">
+						{tags.map((tag, index) => (
+							<Tag key={index} text={tag} size='small' theme='white' isActive={false} />
+						))}
+					</div>
+					<div className="ArticleInfo-title text-title-l">{title}</div>
+					<div className="ArticleInfo-description text-paragraph-m">{description}</div>
+				</div>
+				<div className="infoMain-publushDate text-paragraph-s">{publishDate}</div>
+			</div>
+			<div className="darken-bg"></div>
+		</div>
 	);
 }
